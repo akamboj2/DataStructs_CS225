74144e773bd9f455519b946cef81bf50ab701079
Abhi Kamboj <akamboj2@linux-22.ews.illinois.edu>
2018-09-01 21:17:28 -0500

    makefile tutorial done

--- mp1/cs225/HSLAPixel.cpp
+++ mp1/cs225/HSLAPixel.cpp
@@ -3,9 +3,29 @@
  * Implementation of the HSLAPixel class for use in with the PNG library.
  *
  * @author CS 225: Data Structures
+ * @version 2018r1-lab1
  */
 
 #include "HSLAPixel.h"
+#include <cmath>
+#include <iostream>
+using namespace std;
 
 namespace cs225 {
+  HSLAPixel::HSLAPixel(){
+    s=h=0;
+    a=l=1;
+  }
+  HSLAPixel::HSLAPixel(double hu, double sa, double lu){
+    h=hu;
+    s=sa;
+    l=lu;
+    a=1;
+  }
+  HSLAPixel::HSLAPixel(double hu, double sa, double lu, double al){
+    h=hu;
+    s=sa;
+    l=lu;
+    a=al;
+  }
 }
--- mp1/cs225/HSLAPixel.h
+++ mp1/cs225/HSLAPixel.h
@@ -2,9 +2,23 @@
  * @file HSLAPixel.h
  *
  * @author CS 225: Data Structures
+ * @version 2018r1-lab1
  */
 
-#pragma once
+#ifndef CS225_HSLAPIXEL_H_
+#define CS225_HSLAPIXEL_H_
+
+#include <iostream>
+#include <sstream>
 
 namespace cs225 {
+  class HSLAPixel{
+    public:
+      HSLAPixel();
+      HSLAPixel(double hu, double sa, double lu);
+      HSLAPixel(double hu, double sa, double lu, double al);
+      double h,s,l,a;
+  };
 }
+
+#endif
